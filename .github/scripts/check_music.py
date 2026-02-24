7245abae8a33cc8692d71cccfd990411270ca677
Chris J <chrisjones59@googlemail.com>
2026-02-24 10:28:48 +0000

    Added BBC, Guardian search links

--- .github/scripts/check_music.py
+++ .github/scripts/check_music.py
@@ -36,6 +36,11 @@ else:
             """)
         search_url = f"https://www.google.com/search?q=Identify+for+{country['name'].replace(' ', '+')}:+a+traditional+music+genre,+a+modern+mainstream+artist,+and+an+experimental+fusion+act"
         body_parts.append(f'<p><a href="{search_url}">🔍 Explore more music from {country["name"]}</a></p>')
+        
+        bbc_url = f"https://www.google.com/search?q=BBC+Music+Planet+{country['name'].replace(' ', '+')}"
+        guardian_url = f"https://www.google.com/search?q=Guardian+Global+album+of+the+month+{country['name'].replace(' ', '+')}"
+        body_parts.append(f'<p><a href="{bbc_url}">🎙️ BBC Music Planet - {country["name"]}</a></p>')
+        body_parts.append(f'<p><a href="{guardian_url}">📰 Guardian Global album of the month - {country["name"]}</a></p>')
 
     body = "\n".join(body_parts)
 
